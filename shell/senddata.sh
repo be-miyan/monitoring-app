b15efe6865e9ac2314f5f25d4e59076cdd3e96ac
be-miyan <82134536+be-miyan@users.noreply.github.com>
2021-04-08 16:26:38 +0900

    Update senddata.sh

--- shell/senddata.sh
+++ shell/senddata.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 CSV=${HOME}/rpz-sensor/history/rpz-sensor.csv
-SHELL_DIR=${HOME}/rpz-sensor/shell/
+SHELL_DIR=${HOME}/rpz-sensor/shell
 
 # get latest data
-# rpz_sensor.pyは使用するセンサーによって自作してください(${CSV}に結果出力)
+# rpz_sensor.pyは indoorcorgielec.com 様のサンプルを使用させていただきました
+# (https://www.indoorcorgielec.com/products/rpz-ir-sensor/)
 python3 /home/pi/rpz-sensor/python3/rpz_sensor.py -l $CSV
 
 # create json from csv
 # mapping.jq はrpz_sensor.pyが出力するCSVに応じて編集してください
-tail -n 1 $CSV | jq -R -s -f ${SHELL_DIR}mapping.jq \
-               | jq 'del(.[][] | nulls)'|head -n -2 \
+tail -n 1 $CSV | jq -R -s -f ${SHELL_DIR}/mapping.jq \
+               | jq 'del(.[][] | nulls)'| head -n -2 \
                | sed -e 1d -e 's/},/}/g' | jq . -c \
                | sed -r "s/([0-9]{4})\/([0-9]{2})\/([0-9]{2}) ([0-9]{2}:[0-9]{2})/\1-\2-\3T\4/g" \
                > ${SHELL_DIR}regist.json
